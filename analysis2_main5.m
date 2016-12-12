94d2d9d39cfa33beb1f099a02c612e037f241546
thylakoids <liyulongsjtu@hotmail.com>
2016-12-12 14:03:49 +0800

    del single2comb.m

--- analysis2_main5.m
+++ analysis2_main5.m
@@ -1,10 +1,11 @@
 clc
 clear
 close all
-
+%%
 measure_matrix1=xlsread('original_data_summary\generation4.xls',1,'A1:CV27');
 drug_7=measure_matrix1(7,:);
 drug_7=find(drug_7);
+N=28;
 N_item=nchoosek(1:28,3);
 cutvalue=[0.33 0.33];
 
@@ -16,4 +17,5 @@ plot(1:27,measure_result,'-*')
 title('28 drugs')
 L1=line([1 27],[cutvalue(1) cutvalue(1)]);
 L2=line([1 27],[cutvalue(2) cutvalue(2)]);
-
+%% construct measurement matrix A
+A=[]
